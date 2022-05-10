d40f771ac69d26a66845858a32a1c1e110a8f01b
Kersen1 <92866948+Kersen1@users.noreply.github.com>
2022-05-11 00:44:20 +0200

    Fixed Rows
    fixed rows ... it's no more a square n*n

--- my-game/src/Tiles.js
+++ my-game/src/Tiles.js
@@ -1,18 +1,19 @@
 import React from "react";
 
 export default function Tiles({length, state}) {
+    const lengthOfRows = 5
     const array = [...state.word, ...new Array(length - state.word.length).fill("     ")]
     array[state.word.length] = state.key.join("")
 
     const getColumn = (el) => {
         const result = [];
-        for (let i = 0; i < length; i++) {
+        for (let i = 0; i < lengthOfRows; i++) {
             result.push(<div key={i} className="column">{el[i] || ""}</div>)
         }
 
         return result
     }
-    console.log(array, "dupa")
+    console.log(array)
     return (
         <>
             {array.map((index, i) =>
